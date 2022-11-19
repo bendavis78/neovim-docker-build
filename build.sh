974b1923d156a1ee5dd082613aa0aec4a8a9d5db
Ben Davis <bdavis@oreilly.com>
2022-11-18 21:30:51 -0600

    allow overriding ARCH and VERSION

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-ARCH=arm64
+dir="$(dirname $0)";
+default_arch="$(arch)";
+default_version="$(cd $dir/src/neovim && git describe --tags | sed -e 's/^v//' || exit 1)"
 
-dir=$(dirname $0)
-VERSION="$(cd $dir/src/neovim && git describe --tags | sed -e 's/^v//' || exit 1)"
+ARCH="${ARCH:-$default_arch}"
+VERSION="${VERSION:-$default_version}"
+
+echo "ARCH: $ARCH; VERSION: $VERSION"
+exit 0;
 
 echo "Building neovim-${VERSION}-${ARCH}..."
 docker build --build-arg VERSION=${VERSION} --build-arg ARCH=${ARCH} -t nvim-build . &&
